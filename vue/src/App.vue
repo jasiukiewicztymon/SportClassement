9428daeeeb0727f57cfc78a617bacf1243b10a1b
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-03-08 23:34:06 +0100

    Add files via upload

---
+++ vue/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <a href="" class="text-red-400">cool</a>
+  </div>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import './assets/tailwind.css';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
