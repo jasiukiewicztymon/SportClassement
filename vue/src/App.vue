7b8a1163ec5f954cf5a9c6275ba27913e9d6cdf0
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-03-10 07:49:16 +0100

    Update App.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="absolute w-screen h-screen t-0 l-0">
-    <MenuElement/>
+    <MenuElement @open="changeid"></MenuElement>
   </div>
-  <div class="absolute w-screen h-screen t-0 l-0 none">
-    <TournamentElement/>
+  <div class="absolute w-screen h-screen t-0 l-0" v-if="id != -1">
+    <TournamentElement :id="id" @id="changeid"></TournamentElement>
   </div>
 </template>
 
@@ -14,9 +14,19 @@ import TournamentElement from './components/TournamentElement.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      id: -1,
+    };
+  },
   components: {
     MenuElement,
     TournamentElement
+  },
+  methods: {
+    changeid: function(id) {
+      this.id = id;
+    },
   }
 };
 </script>
