7c2cac54b1df4c48dc795a5b9facf063afb41a0d
titi_2115 <73474137+jasiukiewicztymon@users.noreply.github.com>
2022-03-22 08:26:44 +0100

    Update App.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="absolute w-screen h-screen t-0 l-0">
-    <MenuElement @open="changeid"></MenuElement>
+    <AdminPage></AdminPage>
+  </div>
+  <div class="absolute w-screen h-screen t-0 l-0">
+    <MenuElement @open="changeid" @connexion="adminchange"></MenuElement>
   </div>
   <div class="absolute w-screen h-screen t-0 l-0" v-if="id != -1">
     <TournamentElement :id="id" @id="changeid"></TournamentElement>
@@ -11,22 +14,31 @@
 import './assets/tailwind.css';
 import MenuElement from './components/MenuElement.vue';
 import TournamentElement from './components/TournamentElement.vue'
+import AdminPage from './components/AdminPage.vue'
 
 export default {
   name: 'App',
   data() {
     return {
       id: -1,
+      connected: false,
     };
   },
   components: {
     MenuElement,
-    TournamentElement
+    TournamentElement,
+    AdminPage
   },
   methods: {
     changeid: function(id) {
       this.id = id;
     },
+    adminchange: function() {
+      this.connected = true;
+    },
+    closeadmin: function() {
+      this.connected = false;
+    }
   }
 };
 </script>
